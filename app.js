f5c215aaf8ff9a933c575ce592a429cfc8246af6
SOunit <workbookwork7@gmail.com>
2020-11-03 19:34:13 +0900

    fix adding product path

--- app.js
+++ app.js
@@ -7,8 +7,7 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// app.use('/admin', adminRoutes);
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
