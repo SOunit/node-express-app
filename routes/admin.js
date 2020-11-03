1eb18a93d690b4a0aa2c25d1aaa7b194a36acdab
SOunit <workbookwork7@gmail.com>
2020-11-03 19:26:16 +0900

    fix to serve html pages from server

--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,19 @@
+const path = require('path');
 const express = require('express');
 
 const router = express.Router();
 
 // get /admin/add
-router.get('/add', (req, res, next) => {
-  res.send(
-    '<form action="/admin/add" method="POST"><input type="text" name="title" /><input type="submit" /></form>'
-  );
+router.get('/add-product', (req, res, next) => {
+  // path join is special method, different from just concat strings
+  // path join creates path string for both windows and linux
+  // windows : sample/test/some.html
+  // linux : sample\test\some.html
+  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
 
 // post /admin/add
-router.post('/add', (req, res, next) => {
+router.post('/add-product', (req, res, next) => {
   console.log(req.body);
   res.redirect('/');
 });
--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,10 @@
+const path = require('path');
 const express = require('express');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.send('hello from middleware');
+  res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
 });
 
 module.exports = router;
