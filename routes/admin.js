06f688ee9cafe6c14080183a2d49df20e5c92f23
SOunit <workbookwork7@gmail.com>
2020-11-03 19:51:49 +0900

    change getting root path code

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 
+const rootDir = require('../util/path');
 const router = express.Router();
 
 // get /admin/add
@@ -9,7 +10,12 @@ router.get('/add-product', (req, res, next) => {
   // path join creates path string for both windows and linux
   // windows : sample/test/some.html
   // linux : sample\test\some.html
-  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+
+  // without helper
+  // res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+
+  // with helpler
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 // post /admin/add
